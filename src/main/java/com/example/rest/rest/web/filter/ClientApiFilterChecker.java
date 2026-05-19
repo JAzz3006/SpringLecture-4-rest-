ffa57377d2899b3f1fa8b5939d47eb7cbfa5891d
Grigoriy Chernykh <che90@mail.ru>
2026-05-19 16:45:55 +0500

    studying a little 2

--- src/main/java/com/example/rest/rest/web/filter/ClientApiFilterChecker.java
+++ src/main/java/com/example/rest/rest/web/filter/ClientApiFilterChecker.java
@@ -8,24 +8,22 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import java.io.IOException;
 
 @Component
-@ConditionalOnExpression("${app.check-client-api-header: true}")
+@ConditionalOnExpression("${app.check-client-api-header:true}")
 public class ClientApiFilterChecker extends OncePerRequestFilter {
     private static final String HTTP_CLIENT_HEADER = "X-Client-Api-Key";
-    @Value("${app-client-api-key}")
+    @Value("${app.client-api-key}")
     private String clientApiKey;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String headerValue = request.getHeader(HTTP_CLIENT_HEADER);
         if (headerValue == null || !headerValue.equals(clientApiKey)){
             response.setHeader(HTTP_CLIENT_HEADER, "Invalid");
-            response.sendError(HttpStatus.BAD_GATEWAY.value(), "Header X-Client-Api-Key is wrong or absent");
+            response.sendError(HttpStatus.BAD_REQUEST.value(), "Header X-Client-Api-Key is wrong or absent");
             return;
         }
         filterChain.doFilter(request, response);
-
     }
 }
